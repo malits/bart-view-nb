17577a2938f710d38d818971f29b6574b681388c
malits <maxlitster@gmail.com>
2020-05-21 12:22:08 -0400

    naming

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="bart-view-widget-mrirecon",
+    name="bart-view-widget",
     version="0.0.1",
     description="Jupyter port of Matplotlib-based BART viewer"
 )
